cfc25272cb82669e24b48e80084ead84776e9466
LuisCampillo19 <lgcampillo19@gmail.com>
2025-11-07 09:47:15 -0500

    feat: creation options menu conditional

---
+++ tallerBasico.py/conditional.py
@@ -0,0 +1,25 @@
+def showMenuCo():
+    print("-------------------- MENÚ PRINCIPAL -----------------------")
+    print("1. Mayor de edad")
+    print("2. Número positivo, negativo o cero")
+    print("3. Par o impar")
+    print("4. Calculadora básica")
+    print("5. Clasificador de notas")
+    print("6. Comparador de tres números: mayor y menor")
+    print("7. Salir")
+
+while True:
+    showMenuCo()
+        
+    option = input("Ingresa una opción del 1 al 7: ")
+
+    if option == "1":
+        
+    # elif option == "2":
+    # elif option == "3":
+    # elif option == "4":
+    # elif option == "5":
+    # elif option == "6":
+    # elif option == "7":
+    # else:
+        print("Opción no válida. Intenta de nuevo")
