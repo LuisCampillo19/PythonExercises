8ab9bf13d0035a7ebbd43a11e0a48604ef5f6b18
LuisCampillo19 <lgcampillo19@gmail.com>
2025-11-07 10:46:00 -0500

    feat: add function basic calculator menu

--- tallerBasico.py/conditional.py
+++ tallerBasico.py/conditional.py
@@ -34,6 +34,52 @@ def evenOrOddNumber():
     else:
         print(f"el número {number} es impar\n\n")
 
+def basicCalculator():
+
+    while True:
+        print("------ Calculadora Básica --------")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Volver al menú principal")
+
+        opt = input("Ingresa una opción: ")
+
+        if opt == "1":
+            a = int(input("Ingresa un número: "))
+            b = int(input("Ingresa un número: "))
+
+            c = a + b
+            print(f"la suma de {a} + {b} = {c}\n\n")
+        elif opt == "2":
+            a = int(input("Ingresa un número: "))
+            b = int(input("Ingresa un número: "))
+
+            c = a - b
+            print(f"la resta de {a} - {b} = {c}\n\n")            
+        elif opt == "3":
+            a = int(input("Ingresa un número: "))
+            b = int(input("Ingresa un número: "))
+
+            c = a * b
+            print(f"la multiplicación de {a} x {b} = {c}\n\n")   
+        elif opt == "4":
+            a = int(input("Ingresa un número: "))
+            b = int(input("Ingresa un número: "))
+
+            if b != 0:
+                c = a / b
+                print(f"la división de {a} / {b} = {c}\n\n")
+            else:
+                print("No se puede dividir entre 0")
+        elif opt == "5":
+            print("Saliendo de la calculadora básica...\n\n")
+            break
+        else:
+            print("Opción invalida. Intenta de nuevo\n")
+
+
 while True:
     showMenuCo()
         
@@ -45,7 +91,8 @@ while True:
         numberPoNe()
     elif option == "3":
         evenOrOddNumber()
-    # elif option == "4":
+    elif option == "4":
+        basicCalculator()
     # elif option == "5":
     # elif option == "6":
     # elif option == "7":
