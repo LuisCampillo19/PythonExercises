8ca508581825f2e9b498d4cf27c1f4a01c674d48
LuisCampillo19 <lgcampillo19@gmail.com>
2025-11-07 09:57:37 -0500

    feat: add function legal age validation

--- tallerBasico.py/conditional.py
+++ tallerBasico.py/conditional.py
@@ -8,18 +8,31 @@ def showMenuCo():
     print("6. Comparador de tres números: mayor y menor")
     print("7. Salir")
 
+def legalAge():
+    age = int(input("¿Cuántos años tienes? "))
+
+    if age >17 and age <99:
+        print("Eres mayor de edad\n\n")
+    else:
+        print("Eres menor de edad\n\n")
+
+def numberPoNe():
+    number = int(input("¿Qué número quieres validar?"))
+
 while True:
     showMenuCo()
         
     option = input("Ingresa una opción del 1 al 7: ")
 
     if option == "1":
-        
+        legalAge()
     # elif option == "2":
     # elif option == "3":
     # elif option == "4":
     # elif option == "5":
     # elif option == "6":
     # elif option == "7":
+        print("Saliendo...")
+        #break
     # else:
         print("Opción no válida. Intenta de nuevo")
