0ff5e8278c45c922bae0d08751e7dd41c1eb0768
LuisCampillo19 <lgcampillo19@gmail.com>
2025-11-07 11:30:59 -0500

    feat: add function show menu loops

---
+++ tallerBasico.py/LoopsAndRepetition.py
@@ -0,0 +1,33 @@
+def showMenuLoops():
+    print("-------------------- MENÚ PRINCIPAL -----------------------")
+    print("1. Contar del 1 al 10")
+    print("2. Sumatoria del 1 al n")
+    print("3. Tabla de multiplicar")
+    print("4. Contador regresivo con while")
+    print("5. Adivina el número")
+    print("6. sumar hasta que el usuario escriba 0")
+    print("7. Salir")
+
+
+while True:
+    showMenuLoops()
+        
+    option = input("Ingresa una opción del 1 al 7: ")
+
+    # if option == "1":
+    #     legalAge()
+    # elif option == "2":
+    #     numberPoNe()
+    # elif option == "3":
+    #     evenOrOddNumber()
+    # elif option == "4":
+    #     basicCalculator()
+    # elif option == "5":
+    #     gradeNotes()
+    # elif option == "6":
+    #     compareNumbers()
+    # elif option == "7":
+    #     print("Saliendo...")
+    #     break
+    # else:
+    #     print("Opción no válida. Intenta de nuevo")
