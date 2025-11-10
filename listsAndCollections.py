36105a942efe3aebb765208e87fc8ca7f886ab2e
LuisCampillo19 <lgcampillo19@gmail.com>
2025-11-10 06:55:43 -0500

    feat: creation options menu lists and colletions

---
+++ listsAndCollections.py
@@ -0,0 +1,29 @@
+def showMenuLoops():
+    print("-------------------- MENÚ PRINCIPAL -----------------------")
+    print("1. Lista de frutas")
+    print("2. Agregar y eliminar frutas")
+    print("3. Buscar un elemento en la lista")
+    print("4. Lista de números y promedio")
+    print("5. Números pares: guardar solo los pares")
+    print("6. Eliminar duplicados")
+    print("7. Salir")
+
+while True:
+    showMenuLoops()
+    option = input("Selecciona una opción: ")
+
+    if option == "1":
+        print()
+    elif option == "2":
+        print()
+    elif option == "3":
+        print()
+    elif option == "4":
+        print()
+    elif option == "5":
+        print()
+    elif option == "6":
+        print()
+    elif option == "7":
+        print("Saliendo...")
+        break
